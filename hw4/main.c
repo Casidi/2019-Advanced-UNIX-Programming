b27680ed0a17f9313506ecb6982dd614b75e2e01
Jeff ESXi VM <tcfsh22215@gmail.com>
2019-06-05 14:31:12 +0800

    Add the basic logic for run and vmmap commands

--- hw4/main.c
+++ hw4/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/ptrace.h>
+#include <sys/wait.h>
 #include <string.h>
 #include "elftool.h"
 
@@ -20,49 +23,57 @@ char* help_msg = "- break {instruction-address}: add a break point\n"
 	"- si: step into instruction\n"
 	"- start: start the program and stop at the first instruction\n";
 
-void load_program(char* filename) {
-	elf_handle_t *eh = NULL;
-	if((eh = elf_open(filename)) == NULL) {
-		fprintf(stderr, "** unabel to open '%s'.\n", filename);
+char filename[64] = "";
+
+void load_program(char* new_filename, elf_handle_t** eh, elf_strtab_t** tab) {
+	if(*eh) {
+		elf_close(*eh);
+		*eh = NULL;
+	}
+
+	if((*eh = elf_open(new_filename)) == NULL) {
+		fprintf(stderr, "** unabel to open '%s'.\n", new_filename);
 		return;
 	}
 
-	if(elf_load_all(eh) < 0) {
-		fprintf(stderr, "** unable to load '%s.\n", filename);
+	if(elf_load_all(*eh) < 0) {
+		fprintf(stderr, "** unable to load '%s.\n", new_filename);
 		return;
 	}
 
-	elf_strtab_t *tab = NULL;
-	for(tab = eh->strtab; tab != NULL; tab = tab->next) {
-		if(tab->id == eh->shstrndx) 
+	for(*tab = (*eh)->strtab; *tab != NULL; *tab = (*tab)->next) {
+		if((*tab)->id == (*eh)->shstrndx) 
 			break;
 	}
-	if(tab == NULL) {
+	if(*tab == NULL) {
 		fprintf(stderr, "** section header string table not found.\n");
 		return;
 	}
 
-	for(int i = 0; i < eh->shnum; i++) {
-		if(strcmp(&tab->data[eh->shdr[i].name], ".text") == 0) {
-			printf("** program '%s' loaded. entry point 0x%lx, vaddr 0x%llx, offset 0x%llx, size 0x%llx\n",
-				filename, eh->entrypoint, eh->shdr[i].addr, eh->shdr[i].offset, eh->shdr[i].size);
+	for(int i = 0; i < (*eh)->shnum; i++) {
+		if(strcmp(&(*tab)->data[(*eh)->shdr[i].name], ".text") == 0) {
+			printf("** program '%s' loaded. entry point 0x%lx, "
+				"vaddr 0x%llx, offset 0x%llx, size 0x%llx\n",
+				filename, (*eh)->entrypoint, (*eh)->shdr[i].addr, 
+				(*eh)->shdr[i].offset, (*eh)->shdr[i].size);
 			break;
 		}
 	}
-	if(eh) {
-		elf_close(eh);
-		eh = NULL;
-	}
+
+	strcpy(filename, new_filename);
 }
 
 int main(int argc, char** argv) {
 	size_t line_max = 256;
 	char* line = malloc(line_max);
 	char command[64];
+	pid_t pid_tracee = -1;
+	elf_handle_t *eh = NULL;
+	elf_strtab_t *tab = NULL;
 
 	elf_init();
 	if(argc == 2) {
-		load_program(argv[1]);
+		load_program(argv[1], &eh, &tab);
 	}
 
 	while(1) {
@@ -74,19 +85,66 @@ int main(int argc, char** argv) {
 		} else if(strcmp(command, "delete") == 0) {
 		} else if(strcmp(command, "disasm") == 0) {
 		} else if(strcmp(command, "dum") == 0) {
-		} else if(strcmp(command, "exit") == 0) {
+		} else if(strcmp(command, "exit") == 0 || strcmp(command, "q") == 0) {
 			break;
 		} else if(strcmp(command, "get") == 0) {
 		} else if(strcmp(command, "getregs") == 0) {
-		} else if(strcmp(command, "help") == 0) {
+		} else if(strcmp(command, "help") == 0 || strcmp(command, "h") == 0) {
 			printf("%s", help_msg);
 		} else if(strcmp(command, "list") == 0) {
 		} else if(strcmp(command, "load") == 0) {
-			char filename[64];
-			sscanf(line, "%s %s", command, filename);
-			load_program(filename);
+			char new_filename[64];
+			sscanf(line, "%s %s", command, new_filename);
+			load_program(new_filename, &eh, &tab);
 		} else if(strcmp(command, "run") == 0) {
+			if(strcmp(filename, "") == 0) {
+				printf("No program loaded\n");
+				continue;
+			}
+
+			if((pid_tracee = fork()) < 0) {
+				perror("Failed to fork");
+				continue;
+			}
+			if(pid_tracee == 0) {
+				if(ptrace(PTRACE_TRACEME, 0,0,0) < 0) {
+					perror("ptrace failed");
+					return -1;
+				}
+				execlp(filename, filename, NULL);
+				printf("'%s'\n", filename);
+				perror("execlp");
+				return -1;
+			} else {
+				int status;
+				if(waitpid(pid_tracee, &status, 0) < 0) {
+					printf("waitpid failed\n");
+					continue;
+				}
+				//assert(WIFSTOPPED(status));
+				ptrace(PTRACE_SETOPTIONS, pid_tracee, 0, PTRACE_O_EXITKILL);
+				ptrace(PTRACE_CONT, pid_tracee, 0, 0);
+				waitpid(pid_tracee, &status, 0);
+				perror("done");
+			}
 		} else if(strcmp(command, "vmmap") == 0) {
+			if(strcmp(filename, "") == 0) {
+				printf("No program loaded\n");
+				continue;
+			} else {
+				if(pid_tracee == -1) {
+					//not running
+					for(int i = 0; i < eh->shnum; i++) {
+						if(strcmp(&tab->data[eh->shdr[i].name], ".text") == 0) {
+							printf("%016llx-%016llx r-x %llx       %s\n",
+								eh->shdr[i].addr, eh->shdr[i].addr+eh->shdr[i].size,
+								eh->shdr[i].offset, filename);
+							break;
+						}
+					}
+				} else {
+				}
+			}
 		} else if(strcmp(command, "set") == 0) {
 		} else if(strcmp(command, "si") == 0) {
 		} else if(strcmp(command, "start") == 0) {
