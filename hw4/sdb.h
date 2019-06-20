17557f2bf8cd2a76cac3dccaf2126b1dd446bb29
Jeff ESXi VM <tcfsh22215@gmail.com>
2019-06-20 23:33:21 +0800

    [Add] Pass the first 2 test cases in the spec

--- hw4/sdb.h
+++ hw4/sdb.h
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/ptrace.h>
 #include <sys/wait.h>
+#include <sys/user.h>
 #include "elftool.h"
 
 typedef struct {
@@ -58,7 +59,45 @@ void sdb_load(SDB* sdb, char* new_filename) {
 		}
 	}
 
+	sdb->pid = -1;
 	strcpy(sdb->path, new_filename);
+}
+
+void print_vm_maps(char* line) {
+	char* token = strtok(line, " \n");
+	int count = 0;
+	while(token != NULL) {
+		switch(count) {
+		case 0: {
+			char *second = strchr(token, '-');
+			*second = 0;
+			second++;
+			unsigned long long begin, end;
+			sscanf(token, "%llx", &begin);
+			sscanf(second, "%llx", &end);
+			printf("%016llx-%016llx ", begin, end);
+			break;
+		}
+		case 1:
+			token[3] = 0;
+			printf("%s ", token);
+			break;
+		case 2: {
+			unsigned int offset;
+			sscanf(token, "%x", &offset);
+			printf("%-8x ", offset);
+			break;
+		}
+		case 5:
+			printf("%s ", token);
+			break;
+		default:
+			break;
+		}
+		count++;
+		token = strtok(NULL, " \n");
+	}
+	puts("");
 }
 
 void sdb_vmmap(SDB* sdb) {
@@ -79,14 +118,22 @@ void sdb_vmmap(SDB* sdb) {
 				}
 			}
 		} else {
+			char proc_info_path[64];
+			char* line = NULL;
+			size_t len = 0;
+			sprintf(proc_info_path, "/proc/%d/maps", sdb->pid);
+			FILE* fp = fopen(proc_info_path, "r");
+			while(getline(&line, &len, fp) != -1) {
+				print_vm_maps(line);
+			}
+			if(line)
+				free(line);
+			fclose(fp);
 		}
 	}
 }
 
-void sdb_start() {
-}
-
-void sdb_run(SDB* sdb) {
+void sdb_start(SDB* sdb) {
 	if(strcmp(sdb->path, "") == 0) {
 		printf("No program loaded\n");
 		return;
@@ -111,10 +158,71 @@ void sdb_run(SDB* sdb) {
 			printf("waitpid failed\n");
 			return;
 		}
-		//assert(WIFSTOPPED(status));
 		ptrace(PTRACE_SETOPTIONS, sdb->pid, 0, PTRACE_O_EXITKILL);
-		ptrace(PTRACE_CONT, sdb->pid, 0, 0);
-		waitpid(sdb->pid, &status, 0);
-		perror("done");
+		printf("** pid %d\n", sdb->pid);
+	}
+}
+
+void sdb_get(SDB* sdb, char* reg_name) {
+	struct user_regs_struct regs;
+	ptrace(PTRACE_GETREGS, sdb->pid, 0, &regs);
+	printf("%s = %llu (0x%llx)\n", reg_name, regs.rip, regs.rip);
+}
+
+void sdb_getregs(SDB* sdb) {
+	struct user_regs_struct regs;
+	ptrace(PTRACE_GETREGS, sdb->pid, 0, &regs);
+	printf("RAX %-18llxRBX %-18llxRCX %-18llxRDX %-18llx\n", regs.rax, regs.rbx, regs.rcx, regs.rdx);
+	printf("R8  %-18llxR9  %-18llxR10 %-18llxR11 %-18llx\n", regs.r8, regs.r9, regs.r10, regs.r11);
+	printf("R12 %-18llxR13 %-18llxR14 %-18llxR15 %-18llx\n", regs.r12, regs.r13, regs.r14, regs.r15);
+	printf("RDI %-18llxRSI %-18llxRBP %-18llxRSP %-18llx\n", regs.rdi, regs.rsi, regs.rbp, regs.rsp);
+	printf("RIP %-18llxFLAGS %016llx\n", regs.rip, regs.eflags);
+}
+
+void sdb_run(SDB* sdb) {
+	if(strcmp(sdb->path, "") == 0) {
+		printf("No program loaded\n");
+		return;
+	}
+
+	int status;
+	if(sdb->pid == -1) {
+		if((sdb->pid = fork()) < 0) {
+			perror("Failed to fork");
+			return;
+		}
+		if(sdb->pid == 0) {
+			if(ptrace(PTRACE_TRACEME, 0,0,0) < 0) {
+				perror("ptrace failed");
+				exit(-1);
+			}
+			execlp(sdb->path, sdb->path, NULL);
+			printf("'%s'\n", sdb->path);
+			perror("execlp");
+			exit(-1);
+		} else {
+			if(waitpid(sdb->pid, &status, 0) < 0) {
+				printf("waitpid failed\n");
+				return;
+			}
+			ptrace(PTRACE_SETOPTIONS, sdb->pid, 0, PTRACE_O_EXITKILL);
+		}
+	} else {
+		printf("** program %s is already running.\n", sdb->path);
+	}
+
+	ptrace(PTRACE_CONT, sdb->pid, 0, 0);
+	waitpid(sdb->pid, &status, 0);
+	if(status == 0)
+		printf("** child process %d terminated normally (code %d)\n", sdb->pid, status);
+	else
+		printf("** child process %d exited with error (code %d)\n", sdb->pid, status);
+	sdb->pid = -1;
+}
+
+void sdb_disasm(SDB* sdb, char* addr) {
+	if(strcmp(addr, "") == 0) {
+		puts("** no addr is given");
+		return;
 	}
 }
--- hw4/test.c
+++ hw4/test.c
@@ -4,13 +4,22 @@ int main(int argc, char** argv) {
 	SDB* sdb = sdb_create();
 	sdb_vmmap(sdb);
 
-	puts("\nFirst case...");
+	puts("\nTest Case 1...");
 	sdb_load(sdb, "sample/hello64");
 	sdb_vmmap(sdb);
 	sdb_start(sdb);
-
-	puts("\nSecond case...");
-	sdb_load(sdb, "sample/guess");
 	sdb_vmmap(sdb);
+	sdb_get(sdb, "rip");
+	sdb_run(sdb);
+
+	puts("\nTest Case 2...");
+	sdb_load(sdb, "sample/hello64");
+	sdb_start(sdb);
+	sdb_getregs(sdb);
+
+	puts("\nTest Case 3...");
+	sdb_load(sdb, "sample/hello64");
+	sdb_disasm(sdb, "");
+
 	return 0;
 }
